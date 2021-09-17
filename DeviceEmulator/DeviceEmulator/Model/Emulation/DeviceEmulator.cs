33511b92164ee5bd286fe9a1e8a49214018f5da2
ldeluigi <lucadelu97@gmail.com>
2021-09-17 11:04:14 +0200

    Add project files

---
+++ DeviceEmulator/DeviceEmulator/Model/Emulation/DeviceEmulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeviceEmulator.Model.Emulation
+{
+    public class DeviceEmulator
+    {
+        public async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+        }
+    }
+}
