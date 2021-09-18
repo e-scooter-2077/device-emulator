7bd84fec3137f46235f5bca7963332c01dd971f3
Simone <Simone.magnani4@studio.unibo.it>
2021-09-18 12:14:00 +0200

    Add new secret "HostName"

--- DeviceEmulator/DeviceEmulator/Web/IotHubConfiguration.cs
+++ DeviceEmulator/DeviceEmulator/Web/IotHubConfiguration.cs
@@ -8,6 +8,7 @@ namespace DeviceEmulator.Web
 {
     public class IotHubConfiguration
     {
-        public string ConnectionString { get; init;  }
+        public string ConnectionString { get; init; }
+        public string HostName { get; init; }
     }
 }
